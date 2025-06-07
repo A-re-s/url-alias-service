e8dab29f1971ea5f99e37dc6ca762f0203190666
deg_da <deg_da@mail.ru>
2025-06-07 06:20:38 +0300

    - added docker-compose - added running tests in docker container - updated makefile for them - improved ci - a little app startup refactoring

--- src/main.py
+++ src/main.py
@@ -1,7 +1,5 @@
-import os
 from contextlib import asynccontextmanager
 
-import uvicorn
 from dotenv import load_dotenv
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
@@ -21,9 +19,9 @@ async def test_db_connection(*args):
         async with engine.begin() as conn:
             await conn.run_sync(Base.metadata.create_all)
         yield
-    except Exception as e:
+    except Exception:
         print("❌DB connection failed")
-        print(e)
+        raise
     finally:
         await engine.dispose()
 
@@ -34,13 +32,14 @@ app = FastAPI(
     version="1.0",
 )
 
-app.include_router(router_v1, prefix="/api")
-app.include_router(redirect_router)
-
 
 @app.get("/ping", tags=["Ping"])
 def health_check():
     return {"message": "pong"}
+
+
+app.include_router(router_v1, prefix="/api")
+app.include_router(redirect_router)
 
 
 @app.exception_handler(SQLAlchemyError)
@@ -49,7 +48,3 @@ async def sqlalchemy_exception_handler(request: Request, exc: SQLAlchemyError):
         status_code=500,
         content={"detail": "Internal server error. Please try again later."},
     )
-
-
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=int(os.getenv("API_PORT", "8000")))
